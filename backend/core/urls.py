c58dc9825d2eef0401a5df44e82d0086649195e0
Oleg Kuzmenko <olegkuzmenko2005@gmail.com>
2023-10-22 18:11:45 +0300

    Some fixed

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns
