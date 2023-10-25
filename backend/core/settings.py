44adbb1ac6513041bfd12a3b60e9d28ee3585d72
Oleg Kuzmenko <olegkuzmenko2005@gmail.com>
2023-10-26 01:58:09 +0300

    Fixed ALOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['91.239.235.190', 'localhost']
 
 # SITE NAME
 
